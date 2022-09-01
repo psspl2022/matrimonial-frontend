1bac871ca8b38d6fe0a57787caddf7c0fc0b7fc8
Surajofficial <surajofficial694@gmail.com>
2022-09-01 16:59:58 +0530

    LatestMatch

--- src/Components/LatestMatch.js
+++ src/Components/LatestMatch.js
@@ -125,10 +125,7 @@ function LatestMatch() {
                             </div>
                             <div className="job-des-dt">
                               <h2 style={{ fontWeight: "200" }}>{item.name}</h2>
-                              {/* <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                                elit. Etiam cursus pulvinar dolor nec...
-                                            </p> */}
+
                               <div className="job-skills">
                                 <span>
                                   Age:{" "}
