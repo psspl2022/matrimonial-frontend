a1a2c0c34f9930cc9f27b5249299e67163a6cc42
surajofficial <surajofficial694@gmail.com>
2022-09-07 17:23:11 +0530

    new coponent created

---
+++ src/Components/common/Datacontainer.jsx
@@ -0,0 +1,80 @@
+
+import PaginationBar from "./PaginationBar";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import React from "react";
+import ProfileSkeleton from "../Dummy Skeleton/ProfileSkeleton";
+import { memo } from "react";
+import Usercard from "./Usercard";
+export function Datacontainer(props) {
+    const loding = () => {
+        return (
+            <div
+                className="desired_section"
+                style={{
+                    width: "100%",
+                    display: "grid",
+                    gridTemplateColumns: "auto auto",
+                    columnGap: "20px",
+                }}
+            >
+                <ProfileSkeleton />
+                <ProfileSkeleton />
+                <ProfileSkeleton />
+                <ProfileSkeleton />
+            </div>
+        );
+    };
+    // const [grid, props.setGrid] = useState(false);
+    return (
+        <>
+            <div className="tab-content">
+                <div className="tab-pane active" id="tab-1">
+                    <div
+                        className="row view-group"
+                        id="products"
+                        style={{
+                            minHeight: "800px",
+                            width: "100%",
+                        }}
+                    >
+                        {" "}
+                        {props.data && props.check > 0 &&
+                            props.data.map((item, index) => (
+                                <Usercard key={index}
+                                    item={item}
+                                    showAllProfiles={props.showAllProfiles}
+                                    index={index}
+                                    name={item.name}
+                                    page={props.page}
+                                    className={`lg-item col-lg-6 col-xs-6 grid-group-item1 ${props.grid == true ? "list-group-item1" : ""
+                                        }`}
+                                />
+                            ))}
+                        {(props.data.length == 0 && props.check > 0 && loding())}
+                        {(props.check == 0) && (
+                            <LazyLoadImage
+                                src="./empty.jpg"
+                                style={{
+                                    objectFit: "contain",
+                                    width: "100%",
+                                    height: "400px",
+                                }}
+                            />
+                        )}
+                    </div>
+
+                    <div className="row view-group" id="products">
+                        <div className="col-12">
+                            <div className="main-p-pagination">
+                                {props.total > 1 && props.key1.length > 1 && props.key1 && (
+                                    <PaginationBar CurrentPage={props.CurrentPage} page={props.key1} setPage={props.setPage} />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default memo(Datacontainer);
--- src/Components/common/Showdata.jsx
+++ src/Components/common/Showdata.jsx
@@ -1,31 +1,11 @@
 import Upgradebanner from "./Upgradebanner";
-import PaginationBar from "./PaginationBar";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import SearchFilters from "../SearchFilters";
 import React from "react";
-import { useState, useEffect, Suspense, memo, } from "react";
-import Usercard from "./Usercard";
-import ProfileSkeleton from "../Dummy Skeleton/ProfileSkeleton";
+import { memo } from "react";
 import Topcat from "./Topcat";
+import Datacontainer from "./Datacontainer";
 export function Showdata(props) {
-    const loding = () => {
-        return (
-            <div
-                className="desired_section"
-                style={{
-                    width: "100%",
-                    display: "grid",
-                    gridTemplateColumns: "auto auto",
-                    columnGap: "20px",
-                }}
-            >
-                <ProfileSkeleton />
-                <ProfileSkeleton />
-                <ProfileSkeleton />
-                <ProfileSkeleton />
-            </div>
-        );
-    };
+
     // const [grid, props.setGrid] = useState(false);
     return (
         <>
@@ -39,54 +19,7 @@ export function Showdata(props) {
                             <Upgradebanner />
                             <div className="main-tabs">
                                 <Topcat title="Find Match" setGrid={props.setGrid} />
-                                <div className="tab-content">
-                                    <div className="tab-pane active" id="tab-1">
-                                        <div
-                                            className="row view-group"
-                                            id="products"
-                                            style={{
-                                                minHeight: "800px",
-                                                width: "100%",
-                                            }}
-                                        >
-                                            {" "}
-                                            {props.data && props.check > 0 &&
-                                                props.data.map((item, index) => (
-                                                    <Usercard key={index}
-                                                        item={item}
-                                                        showAllProfiles={props.showAllProfiles}
-                                                        index={index}
-                                                        name={item.name}
-                                                        page={props.page}
-                                                        className={`lg-item col-lg-6 col-xs-6 grid-group-item1 ${props.grid == true ? "list-group-item1" : ""
-                                                            }`}
-                                                    />
-                                                ))}
-                                            {/* {props.check} */}
-                                            {(props.data.length == 0 && props.check > 0 && loding())}
-                                            {(props.check == 0) && (
-                                                <LazyLoadImage
-                                                    src="./empty.jpg"
-                                                    style={{
-                                                        objectFit: "contain",
-                                                        width: "100%",
-                                                        height: "400px",
-                                                    }}
-                                                />
-                                            )}
-                                        </div>
-
-                                        <div className="row view-group" id="products">
-                                            <div className="col-12">
-                                                <div className="main-p-pagination">
-                                                    {props.total > 1 && props.key1.length > 1 && props.key1 && (
-                                                        <PaginationBar CurrentPage={props.CurrentPage} page={props.key1} setPage={props.setPage} />
-                                                    )}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <Datacontainer data={props.data} key1={props.key1} check={props.check} showAllProfiles={props.showAllProfiles} page={props.page} grid={props.grid} CurrentPage={props.CurrentPage} total={props.total} setPage={props.setPage} />
                                 {/* card use  */}
                             </div>
                         </div>
